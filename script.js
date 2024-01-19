17c0e184e479ac01d9cb251a1233eb28c71def2c
xenoroxy <renasoong@gmail.com>
2024-01-19 13:03:56 +0800

    modify  signUp page

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function signUp() {
     alert("No existing account? Sign Up!");
     window.location.href="signUp.html";
 }
+
+function logIn() {
+    alert("Back to sign in page");
+    window.location.href="logIn.html";
+}
