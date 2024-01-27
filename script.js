5040ecc5f6e931c57efb6d3de8b518e701dc1d91
KurvyHerv <hervsie@gmail.com>
2024-01-27 16:26:50 +0800

    Implemented API (not working due to CORS header error)

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+document.addEventListener("DOMContentLoaded", function() {
+  const APIKEY = "65b471a98b1bd4aa1706fb1a";
+  document.getElementById("loginSubmit").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache"
+      }
+    }
+
+    fetch("https://interactivedev-f11e.restdb.io/rest/student", settings)
+    .then(response => response.json())
+    .then(response => {
+
+      for (var i = 0; i < response.length && i < limit; i++) {
+
+        if (response[i].email == document.getElementById("email").value) {
+          console.log("SDJKLFSDJFSLFDJFSDLKFJSDLSFJS:FLSDFJSD")
         }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+        else {
+          console.log("NOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+        }
+
+      }
+    });
+});
+})
