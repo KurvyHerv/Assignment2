76e6b76face80123480942c005634cb0c1cdc463
KurvyHerv <hervsie@gmail.com>
2024-01-29 14:50:48 +0800

    change api to firebase

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
+const docRef = doc(assignment2, "users", "hervsie@gmail.com");
+const docSnap = await getDoc(docRef);
+
 document.addEventListener("DOMContentLoaded", function() {
-  const APIKEY = "65b471a98b1bd4aa1706fb1a";
-  document.getElementById("loginSubmit").addEventListener("click", function(e) {
-    e.preventDefault();
 
-    let settings = {
-      "async": true,
-      "crossDomain": true,
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-apikey": APIKEY,
-        "Cache-Control": "no-cache"
-      }
-    }
-
-    fetch("https://interactivedev-f11e.restdb.io/rest/student", settings)
-    .then(response => response.json())
-    .then(response => {
-
-      for (var i = 0; i < response.length && i < limit; i++) {
-
-        if (response[i].email == document.getElementById("email").value) {
-          console.log("SDJKLFSDJFSLFDJFSDLKFJSDLSFJS:FLSDFJSD")
-        }
-        else {
-          console.log("NOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-        }
-
-      }
-    });
+if (docSnap.exists()) {
+  console.log("Document data:", docSnap.data());
+} else {
+  // docSnap.data() will be undefined in this case
+  console.log("No such document!");
+}
 });
-})
