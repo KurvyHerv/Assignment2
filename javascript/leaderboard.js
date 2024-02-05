b5009c25e44715f25fab0883fde05f56a021dced
KurvyHerv <hervsie@gmail.com>
2024-02-05 10:29:58 +0800

    added search bar

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -1,5 +1,5 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-  import { getFirestore, getDocs, collection, query, orderBy} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+  import { getFirestore, getDocs, collection, query, orderBy, where} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
   // Firebase configuration
   const firebaseConfig = {
@@ -18,7 +18,8 @@
   async function querySnapshot(q) {
     const querySnapshot = await getDocs(q);
     if (querySnapshot.empty) {
-      console.log("No users for leaderboard")
+      $("#leaderboardList2").append("<p>No users for leaderboard</p>")
+      $("#leaderboardList").append("<p>No users for leaderboard</p>")
     } else {
       querySnapshot.forEach((doc) => {
         $("#leaderboardList2").append('<li class="list-group-item d-flex justify-content-between align-items-start"><div class="ms-2 me-auto"><div class="fw-bold">' + doc.data().username + '</div></div><span class="badge bg-primary rounded-pill">' + doc.data().score + ' Points</span></li>');   
@@ -27,8 +28,33 @@
     }
   }
 
+  async function search(q) {
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      $("#leaderboardList").empty().append("<p>No users for leaderboard</p>")
+    } else {
+      querySnapshot.forEach((doc) => {
+        $("#leaderboardList").empty().append('<li class="list-group-item d-flex justify-content-between align-items-start"><div class="ms-2 me-auto"><div class="fw-bold">' + doc.data().username + '</div></div><span class="badge bg-primary rounded-pill">' + doc.data().score + ' Points</span></li>');   
+    });
+    }
+  }
+
   $( document ).ready(function() {
     $("#accountBtn").text(localStorage.getItem("username"));
     const q = query(collection(db, "users"), orderBy("score", "desc"));
     querySnapshot(q);
+  });
+
+  $("#submit").click(function(){
+    const username = $("#search").val();
+    var q = query(collection(db, "users"), where("username", "==", username.toLowerCase()));
+    if (username == "") {
+      q = query(collection(db, "users"), orderBy("score", "desc"));
+      $("#leaderboardList").empty();
+      $("#leaderboardList2").empty();
+      querySnapshot(q);
+    }
+    else {
+      search(q);
+    }
   });
