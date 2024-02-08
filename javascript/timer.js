e2954e53fccdf86dae84b8f31af9f928c095fb82
KurvyHerv <hervsie@gmail.com>
2024-02-08 18:25:30 +0800

    fixed

--- javascript/timer.js
+++ javascript/timer.js
@@ -3,7 +3,7 @@ import { gameOver } from './load.js';
 // Set the date we're counting down to
 var countDownDate = new Date();
 countDownDate.setMinutes(countDownDate.getMinutes() + 3);
-countDownDate.setSeconds(countDownDate.getSeconds() + 4);
+countDownDate.setSeconds(countDownDate.getSeconds() + 3);
 
 // Update the count down every 1 second
 var x = setInterval(function() {
