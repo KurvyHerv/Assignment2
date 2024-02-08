005629b23f70cf52eba1b267c0f28b9bc2dd5b82
KurvyHerv <hervsie@gmail.com>
2024-02-08 10:31:19 +0800

    changed timer timing

--- javascript/timer.js
+++ javascript/timer.js
@@ -2,8 +2,8 @@ import { gameOver } from './load.js';
 
 // Set the date we're counting down to
 var countDownDate = new Date();
-countDownDate.setMinutes(countDownDate.getMinutes() + 0);
-countDownDate.setSeconds(countDownDate.getSeconds() + 5);
+countDownDate.setMinutes(countDownDate.getMinutes() + 3);
+countDownDate.setSeconds(countDownDate.getSeconds() + 4);
 
 // Update the count down every 1 second
 var x = setInterval(function() {
